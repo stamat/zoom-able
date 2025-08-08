457003c450b369ac85a8a88254b9ba034682653a
Nikola Stamatović <stamat@github.com>
2025-08-08 11:44:32 +0200

    meh I don't know what's this

--- dist/script.js
+++ dist/script.js
@@ -5,12 +5,9 @@
     return /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(str) || /\b(Android|Windows Phone|iPad|iPod)\b/i.test(str);
   }
   function isMobile() {
-    if ("maxTouchPoints" in navigator)
-      return navigator.maxTouchPoints > 0;
-    if ("matchMedia" in window)
-      return !!matchMedia("(pointer:coarse)").matches;
-    if ("orientation" in window)
-      return true;
+    if ("maxTouchPoints" in navigator) return navigator.maxTouchPoints > 0;
+    if ("matchMedia" in window) return !!matchMedia("(pointer:coarse)").matches;
+    if ("orientation" in window) return true;
     return isUserAgentMobile(navigator.userAgent);
   }
 
